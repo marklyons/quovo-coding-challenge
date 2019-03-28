43d75e33b4c7db97f5b670dc602e26b479824dbd
Mark Lyons <mklyons@umd.edu>
2019-03-28 19:57:00 -0400

    Gets text file URL.

--- edgar-search.py
+++ edgar-search.py
@@ -17,7 +17,11 @@ def get_fund_html(CIK):
 	search_html = urllib.request.urlopen(search_url).read()
 	return search_html
 
-def get_13F_HR_html(html_fund_page):
+def get_13F_html(html_fund_page):
+	""" Returns the HTML for the most recent 13F-HR.
+			@param html_fund_page: HTML of the fund's EDGAR page.
+	"""
+
 	# Use BeautifulSoup to parse HTML
 	soup = BeautifulSoup(html_fund_page, features="html.parser")
 
@@ -33,16 +37,25 @@ def get_13F_HR_html(html_fund_page):
 		# We only need the first 13F-HR entry.
 		if(filings_val == "13F-HR"):
 			format_col = row.findAll("td")[1]
-			url_13F_HR = "https://www.sec.gov" + format_col.find("a")['href']
-			html_13F_HR = urllib.request.urlopen(url_13F_HR).read()
-			return html_13F_HR
-			# Because we're only looking for first match:
-			break
+			url_13F = "https://www.sec.gov" + format_col.find("a")['href']
+			html_13F = urllib.request.urlopen(url_13F).read()
+			return html_13F
 
 	# Occurs if first 40 documents don't contain a 13F-HR
 	return None 
 
+def get_13F_text_url(html_13F_page):
+	# Use BeautifulSoup to parse HTML
+	soup = BeautifulSoup(html_13F_page, features="html.parser")
+	documents_table = soup.find("table", {"class": "tableFile"})
+
+	# It looks like it will always be last item.
+	last_row = documents_table.findAll("tr")[-1]
+	url_col = last_row.findAll("td")[2]
+	text_url = "https://www.sec.gov" + url_col.find("a")['href']
+	return text_url
+
 html_fund_page = get_fund_html("0001166559")
-html_13F_HR_page = get_13F_HR_html(html_fund_page)
-print(html_13F_HR_page)
+html_13F_page = get_13F_html(html_fund_page)
+text_url_13F = get_13F_text_url(html_13F_page)
 
