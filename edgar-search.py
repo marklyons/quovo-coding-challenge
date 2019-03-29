1c1a2d349fb22fe234e7d7a48f9c160526a51fac
Mark Lyons <mklyons@umd.edu>
2019-03-28 23:23:25 -0400

    Graphing supported

--- edgar-search.py
+++ edgar-search.py
@@ -1,5 +1,6 @@
 import csv
 import sys
+import argparse
 import urllib.request
 import urllib.parse 
 from bs4 import BeautifulSoup
@@ -75,7 +76,10 @@ def parse_13F_text_file(text_url_13F, CIK):
 	""" Parses 13F_HR .txt and produces a TSV file
 			@param text_url_13F: String contents of the 13F-HR .txt file.
 			@param CIK: The Central Index Key of the desired fund
+			Returns data to be used in graph if option selected.
 	"""
+	graph_data = []
+
 	text_13F = urllib.request.urlopen(text_url_13F).read()
 
 	# BeautifulSoup also works for XML!
@@ -116,18 +120,78 @@ def parse_13F_text_file(text_url_13F, CIK):
 				voting_authority_none
 			])
 
+			# Append to our graph data=
+			graph_data.append([name_of_issuer, int(value)])
+
 	holdings_tsv.close()
+	return graph_data
+
+def create_pie_chart(graph_data, CIK):
+	""" Most funds have way too many holdings to show a complete pie chart.
+			This gathers the top 10 holdings and shows how they compare to each
+			other, as well as the remaining, non top-ten part of the portfolio.
+			@param graph_data: A 2D array of [holding name, value]
+			This triggers a matplotlib pie chart window to open.
+	"""
+
+	total_value = 0
+	top_ten_value = 0
+
+	for holding in graph_data:
+		total_value = total_value + holding[1]
+
+	sorted_data = sorted(graph_data, key=lambda x: x[1], reverse=True)
+	top_ten = sorted_data[:10]
+
+	labels = ()
+	values = []
+
+	# Decompose our now sorted data
+	for holding in top_ten:
+		curr_name = holding[0]
+		curr_value = holding[1]
+		labels = labels + (curr_name,)
+		values.append(curr_value)
+		top_ten_value = top_ten_value + curr_value
+
+	non_top_ten_value = total_value - top_ten_value
+	labels = labels + ("Rest of portfolio",)
+	values.append(non_top_ten_value)
+
+	colors = [
+		'gold', 'yellowgreen', 'lightcoral', 'lightskyblue', 'maroon', 'darkslateblue',
+		'slategray', 'palegreen', 'tomato', 'chocolate', 'deepskyblue'
+	]
+	 
+	# Plot
+	plt.title("Top 10 Holdings of CIK:" + CIK)
+	plt.pie(values, autopct='%1.1f%%', shadow=False, startangle=140, colors = colors)
+	plt.legend(labels, loc="best")
+	plt.axis('equal')
+	plt.show()
 
 
-CIK_in = sys.argv[1]
+parser = argparse.ArgumentParser()
+parser.add_argument("-cik", help="Central Index Key")
+parser.add_argument("-d", help="Date 13F-HR Filed")
+parser.add_argument("-g", help="Draw a graph", action='store_true')
+args = parser.parse_args()
+
+
+CIK_in = args.cik
 
 # Allow user to specify filing date (optional)
-filing_date = None
-if(len(sys.argv) >= 3):
-	filing_date = sys.argv[2]
+filing_date = args.d
+
+# Also allow graphing functionality
+graph = args.g 
 
 html_fund_page = get_fund_html(CIK_in)
 html_13F_page = get_13F_html(html_fund_page, filing_date)
 text_url_13F = get_13F_text_url(html_13F_page)
-parse_13F_text_file(text_url_13F, CIK_in)
+graph_data = parse_13F_text_file(text_url_13F, CIK_in)
 
+if(graph is not None):
+	# Don't want to force mathplotlib for standard use
+	import matplotlib.pyplot as plt
+	create_pie_chart(graph_data, CIK_in)
